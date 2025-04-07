9f83ab8785094ae8d6f80a66c5f54ccda91184b3
Rainer Rodrigues <rainerrodrigues@gmail.com>
2025-04-07 04:35:58 +0100

    Plotting Co2 emissions for the selected countries to see the general trend across them

--- model/EDA.jl
+++ model/EDA.jl
@@ -19,3 +19,20 @@ println("Mean: $mean_emissions, Median: $median_emissions, Std Dev: $std_emissio
 
 plot(us_data.year, us_data.co2, title="US CO₂ Emissions Over Time",
      xlabel="Year", ylabel="CO₂ Emissions (million tonnes)", lw=2)
+
+# Now, I will be investigating on several countries that are currently in conflicts(dated 7/4/25)
+conflict_countries = [
+    "Ukraine","Russia","Isreal","Palestine","Sudan","Syria","Yemen","Armenia","Azerbaijan",
+    "Rwanda","Democratic Republic of Congo"
+]
+
+filtered_df = filter(row -> row.country in conflict_countries && !ismissing(row.co2),df)
+
+# Plot CO₂ trends over time for each country
+grouped = groupby(filtered_df, :country)
+
+for g in grouped
+    country = unique(g.country)[1]
+    plot(g.year, g.co2, label=country, lw=2, xlabel="Year", ylabel="CO₂ Emissions", title="CO₂ Emissions for $country")
+    display(current())  # Show each plot individually
+end
